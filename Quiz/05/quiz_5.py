03ca47c1d5d670a93d85e26f851cff7be14f634e
ruimu <marey111@126.com>
2020-03-19 09:25:29 +1100

    assignment

---
+++ Quiz/05/quiz_5.py
@@ -0,0 +1,16 @@
+# DO *NOT* WRITE YOUR NAME TO MAINTAIN ANONYMITY FOR PLAGIARISM DETECTION
+
+
+# In both functions below, grid is supposed to be a sequence of strings
+# all of the same length, consisting of nothing but spaces and *s,
+# and represent one or more "full polygons" that do not "touch" each other.
+
+def display(*grid):
+    pass
+    # REPLACE pass ABOVE WITH YOUR CODE
+
+def display_leftmost_topmost_boundary(*grid):
+    pass
+    # REPLACE pass ABOVE WITH YOUR CODE
+
+# POSSIBLY DEFINE OTHER FUNCTIONS
