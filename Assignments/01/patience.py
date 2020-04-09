e25c53e53df640e93554236f93d9585cfb3d9c95
ruimu <marey111@126.com>
2020-04-09 20:49:18 +1000

    Quiz05

--- Assignments/01/patience.py
+++ Assignments/01/patience.py
@@ -1,8 +1,10 @@
 from random import seed, shuffle
+from copy import deepcopy
+from collections import defaultdict
 
 
 def generate_dial_and_centre(for_seed):
-    colours = 'CDHS' # Clubs, Diamonds, Hearts, Spades
+    colours = 'CDHS'  # Clubs, Diamonds, Hearts, Spades
     #                                            jacks, queens, kings
     ranks = list(str(x) for x in range(1, 11)) + list('jqk')
     seed(for_seed)
@@ -13,18 +15,95 @@ def generate_dial_and_centre(for_seed):
         dial[i + 1] = [cards[i + 13 * j] for j in range(4)]
     return dial, [cards[12 + 13 * j] for j in range(4)]
 
-def initial_hour(hour, dial):
-    pass
+
+def get_print_card(card, fipped_cards = None):
+    colours = 'CDHS'
+    unicode_colours = "DCBA"
+    generate_cards = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "j", "q", "k"]
+    unicode_cards = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "D", "E"]
+    unicode_base = "\\U0001F0"
+    card_colour = card[0]
+    # 在colours 里面去寻找这个index
+    card_colour_index = colours.find(card_colour)
+    unicode_colour = unicode_colours[card_colour_index]
+    generate_card_number = card[1:]
+    generate_card_index = generate_cards.index(generate_card_number)
+    unicode_card = unicode_cards[generate_card_index]
+    # 三部分相加
+    current_unicode_card = unicode_base + unicode_colour + unicode_card
+    current_unicode_card = bytes(current_unicode_card.encode('utf-8'))
+    print_unicode_card = current_unicode_card.decode('unicode-escape')
+
+    if fipped_cards and card in fipped_cards:
+        return print_unicode_card
+
+    return "hidden" + print_unicode_card
+
+def initial_hour(hour, dial, fipped_cards = None):
     # REPLACE PASS ABOVE WITH YOUR CODE
+    print(" ".join([get_print_card(card, fipped_cards) for card in dial[hour]]))
+
+
+def flipping_cards(new_dial, current_hour, nb_of_steps,
+                   flipped_cards_counter,flipped_cards,
+                   check_king = 0):
+    if nb_of_steps < 0:
+        return
+    # check 翻开的个数
+    if flipped_cards_counter[current_hour] == 4:
+        # 是否所有的牌都翻开了。
+        for value in flipped_cards.values():
+            if len(value) == 4:
+                continue
+            else:
+                if check_king :
+                    raise Exception("No success...")
+                raise Exception("Could not play that far...")
+        else:
+            return
+
+    generate_cards = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "j", "q", "k"]
+    flip_card = new_dial[current_hour].pop()
+    generate_card_number = flip_card[1:]
+    card_number = generate_cards.index(generate_card_number) + 1
+
+    # 把翻开的牌插入进来
+    new_dial[card_number].insert(0,flip_card)
+    # 翻开的hour里面记述
+    flipped_cards_counter[current_hour] += 1
+    # 翻开后的牌，记录下来
+    flipped_cards[card_number].append(flip_card)
+    # 继续执行
+    flipping_cards(new_dial,card_number,nb_of_steps - 1,
+                   flipped_cards_counter, flipped_cards,
+                   check_king)
 
 def hour_after_playing_from_beginning_for_at_most(hour, nb_of_steps, dial,
-                                                  centre
-                                                 ):
-    print()
+                                                  centre,
+                                                  check_king = 0):
     # REPLACE PRINT() ABOVE WITH YOUR CODE
+    new_dial =deepcopy(dial)
+    new_dial[13] = deepcopy(centre)
+    # 当前翻开牌计数器
+    flipped_cards_counter = defaultdict(int)
+    # 翻开的牌
+    flipped_cards = defaultdict(list)
+    try:
+        flipping_cards(new_dial, 13, nb_of_steps, flipped_cards_counter,flipped_cards, check_king)
+        initial_hour(hour, new_dial, flipped_cards[hour])
+    except Exception as e:
+        print(e)
 
 def kings_at_end_of_game(dial, centre):
-    print()
+    hour_after_playing_from_beginning_for_at_most(13, 53, dial,centre, 1)
     # REPLACE PRINT() ABOVE WITH YOUR CODE
 
+
 # POSSIBLY DEFINE OTHER FUNCTIONS
+if __name__ == "__main__":
+    dial, centre = generate_dial_and_centre(0)
+    # initial_hour(7, dial)
+    # hour_after_playing_from_beginning_for_at_most(12, 1, dial, centre)
+    kings_at_end_of_game(dial, centre)
+    dial, centre = generate_dial_and_centre(18)
+    kings_at_end_of_game(dial, centre)
--- Assignments/01/poker_dice.py
+++ Assignments/01/poker_dice.py
@@ -1,13 +1,120 @@
 from random import randint
+from collections import defaultdict
+
+
+def card_hands(random_card_numbers):
+    hand = ''.join(str(i) for i in random_card_numbers)
+
+    types = ["1112", "122", "113", "14", "5", "23"]
+    print_types = ["One pair", "Two pair", "Three of a kind"
+        , "Four of a kind", "Five of a kind", "Full house"]
+
+    hand_type = "Bust"
+    if hand == '01234' or hand == '12345':
+        hand_type = "Straight"
+    else:
+        counter = defaultdict(int)
+        for number in random_card_numbers:
+            counter[number] = counter[number] + 1
+
+        values = list(counter.values())
+        values.sort()
+        str_values = "".join(str(x) for x in values)
+        if str_values in types:
+            hand_type = print_types[types.index(str_values)]
+
+    return hand_type
+
+
+def check_input_cards_not_exist(input_cards, random_cards):
+    for card in input_cards:
+        if card not in random_cards:
+            return True
+
+    return False
+
+
+def check_input_cards_counter(input_cards, random_cards):
+    for card in input_cards:
+        if input_cards.count(card) > random_cards.count(card):
+            return True
+
+    return False
 
 
 def play():
-    pass
     # REPLACE PASS ABOVE WITH YOUR CODE
+    card_orders = ["Ace", "King", "Queen", "Jack", "10", "9"]
+    random_card_numbers = [randint(0, 5) for _ in range(5)]
+    random_card_numbers.sort()
+
+    random_cards = [card_orders[index] for index in random_card_numbers]
+    print(f"The roll is: {' '.join(random_cards)}")
+    print(f"It is a {card_hands(random_card_numbers)}")
+
+    number = 2
+    while (True):
+        if number == 2:
+            input_value = input("Which dice do you want to keep for the second roll? ")
+        else:
+            input_value = input("Which dice do you want to keep for the third roll? ")
+
+        if input_value.lower() in ["all"]:
+            print("Ok, done.")
+            break
+
+        input_cards = input_value.split()
+        if check_input_cards_not_exist(input_cards, random_cards) \
+                and check_input_cards_counter(input_cards, random_cards):
+            print("That is not possible, try again!")
+            continue
+
+        # 如果输入的都保留
+        input_cards_numbers = [card_orders.index(card) for card in input_cards]
+        input_cards_numbers.sort()
+        if input_cards_numbers == random_card_numbers:
+            print("Ok, done.")
+            break
+
+        re_random_card_numbers = [randint(0, 5) for _ in range(5 - len(input_cards))]
+        re_random_card_numbers.sort()
+        re_random_cards = [card_orders[index] for index in re_random_card_numbers]
+
+        random_cards = input_cards + re_random_cards
+
+        random_card_numbers = [card_orders.index(card) for card in random_cards]
+        random_card_numbers.sort()
+        random_cards = [card_orders[index] for index in random_card_numbers]
+        print(f"The roll is: {' '.join(random_cards)}")
+        print(f"It is a {card_hands(random_card_numbers)}")
+        number +=1
 
 def simulate(n):
-    pass
     # REPLACE PASS ABOVE WITH YOUR CODE
+    print_types = ["Five of a kind",
+                   "Four of a kind",
+                   "Full house",
+                   "Straight",
+                   "Three of a kind",
+                   "Two pair",
+                   "One pair"]
 
-    
+    result = defaultdict(int)
+    for _ in range(n):
+        random_card_numbers = [randint(0, 5) for _ in range(5)]
+        random_card_numbers.sort()
+        hand_type = card_hands(random_card_numbers)
+        result[hand_type] =result[hand_type]  + 1
+
+    for key in print_types:
+        print(f"{key:15} : {result[key]/n:2.2%}")
+
+
 # DEFINE OTHER FUNCTIONS
+if __name__ == "__main__":
+    from random import seed
+
+    seed(0)
+    simulate(10)
+    simulate(100)
+
